3589515b6679a18e74f6cc17f26560dab48b8477
Serhii <SERHII.PRYNKEVYCH25@bathspa.ac.uk>
2025-10-14 12:35:15 +0100

    Practice task

--- Petrol Calculator.cpp
+++ Petrol Calculator.cpp
@@ -1,1 +1,26 @@
+#include <iostream> 
+using namespace std; 
 
+int main() 
+{ 
+    double distance, carconsumpt, calculation,priceperlitre,totalcost;
+
+    cout << "Enter distance you need to tavel (in km): ";
+    cin >> distance;
+
+    cout << "Enter petrol price per litre (in GBP): ";
+    cin >> priceperlitre;
+
+    cout << "Enter your car gas consumption (liters/100km): ";
+    cin >> carconsumpt;
+
+    
+
+    calculation = (distance / carconsumpt) ;
+    cout << "Your car's fuel consumption is " << calculation << endl;
+
+    totalcost = calculation * priceperlitre;
+    cout << "Your total fuel cost is " << totalcost << " GBP" << endl;
+
+    return 0; 
+}
