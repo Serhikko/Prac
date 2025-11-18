da47cc161fd41938589ec86d7f4359cb569ba80f
Serhii <SERHII.PRYNKEVYCH25@bathspa.ac.uk>
2025-11-18 12:05:49 +0000

    functions

---
+++ arrays/functions1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+// You say hello, I say goodbye
+
+// Icon to indicate task activity
+
+// Write a program that contains and invokes two functions.
+
+// One named hello that outputs a welcome message which reads "Welcome to my program"
+// One named goodbye that outputs a goodbye message which reads "End of program".
+
+void hello()
+{ // function 
+    cout << "Welcome to my program" << endl;
+}
+
+void goodBye()
+{
+    cout << "End of program" << endl;
+}
+
+int main()
+{
+    hello(); // invoke function (e.g. make it run)
+    goodBye();
+    
+
+    return 0;
+}
